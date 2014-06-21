76886619aec3daa8acc4b9944e211b9886cfd8f6
Sinan SHI <shisinancn@gmail.com>
2014-06-21 02:55:23 +0200

    new method

--- crops2band.r
+++ crops2band.r
@@ -1,50 +1,45 @@
-#prod.af2 Table for investor countries dataset
-#crops.prod.af for target countries dataset
-#Crop asigment 
 croplist<-list()
-croplist[["accacia"]]<- 0
-croplist[["alfalfa"]]<- 24 
-croplist[["banana"]]<- c( 15, 41)
-croplist[["barley"]]<- c( 1, 27)
-croplist[["bean"]]<- c( 5, 31)
-croplist[["cacao"]]<- 0
-croplist[["cassava"]]<- c( 8, 34)
-croplist[["castor.oil"]]<- 0
-croplist[["cereals"]]<- c( 1, 27, 4, 30)
-croplist[["citrus"]]<- c( 14,40)
-croplist[["coffee"]]<- 0
-croplist[["corn"]]<- c( 3,29)
-croplist[["cotton"]]<- c( 21, 47)
-croplist[["eucalyptus"]]<- c( 26, 52)# (Note!!, no data in the input, so asign where agriculture exists)
-croplist[["flowers"]]<- c(23,49)
-croplist[["food.crops"]]<- c( 1:3, 27:29)
-croplist[["fruit"]]<- c( 15,41)
-croplist[["grains"]]<- c( 1,27)
-croplist[["grapes"]]<- c( 19,45)
-croplist[["jatropha"]]<- 0
-croplist[["not.stated"]]<- c( 23,49)
-croplist[["oil.palm"]]<- 0
-croplist[["oil.seeds"]]<- 0
-croplist[["onion"]]<- c( 23,49)
-croplist[["palms"]]<- 0
-croplist[["peanut"]]<- c( 11,37)
-croplist[["peas"]]<- c( 5,31)
-croplist[["pepper"]]<- c( 23,49)
-croplist[["pine"]]<- 0
-croplist[["pineapple"]]<- c( 23,49)
-croplist[["potatoes"]]<- c( 7,33)
-croplist[["rice"]]<- c( 2,28)
-croplist[["rubber"]]<- 0
-croplist[["sesame"]]<- c( 23,29)
-croplist[["sorghum"]]<- c( 4,30)
-croplist[["soybeans"]]<- c( 10,36)
-croplist[["sugar.cane"]]<- c( 13,39)
-croplist[["sunflower"]]<- c( 9,35)
-croplist[["tea"]]<- 0
-croplist[["teak"]]<- 0
-croplist[["tomatoes"]]<- c( 24,50)
-croplist[["trees"]]<- 0
-croplist[["vegetables"]]<- c( 23,49)
-croplist[["wheat"]]<- c( 1,27)
-
-
+croplist[["accacia"]]<- list(c(0),c(0)) #0 is for natural vegetation
+croplist[["alfalfa"]]<- list(c(22,48),c(24,50)) # fodder as indicator, asign to managed grass  
+croplist[["banana"]]<- list(c(15,41),c(15,41)) #orchards
+croplist[["barley"]]<- list(c(1,27),c(1,27)) #temp cer
+croplist[["bean"]]<- list(c(5,31),c(5,31)) # pulses
+croplist[["cacao"]]<- list(c(0),c(0))
+croplist[["cassava"]]<- list(c(8,34),c(8,34)) #trop roots
+croplist[["castor.oil"]]<- list(c(0),c(0))
+croplist[["cereals"]]<- list(c(4,30),c(4,30)) # tropical cer because in Congo
+croplist[["citrus"]]<- list(c(14,40),c(14,40)) 
+croplist[["coffee"]]<- list(c(0),c(0))
+croplist[["corn"]]<- list(c(3,29),c(3,29)) #maize
+croplist[["cotton"]]<- list(c(21,47),c(21,47)) 
+croplist[["eucalyptus"]]<- list(c(15,41),c(26,52)) # orchards as indicator, asign to bioenergy tree
+croplist[["flowers"]]<- list(c(23,49),c(23,49)) # others
+croplist[["food.crops"]]<- list(c(1:4, 27:30),c(1:4, 27:30)) # temp cereals, rice, corn, trop cer
+croplist[["fruit"]]<- list(c(15,41),c(15,41)) #orchards
+croplist[["grains"]]<- list(c(1,27),c(1,27)) # temp cereals
+croplist[["grapes"]]<- list(c(19,45),c(19,45)) 
+croplist[["jatropha"]]<- list(c(0),c(0))
+croplist[["not.stated"]]<- list(c(23,49),c(23,49)) #others
+croplist[["oil.palm"]]<- list(c(0),c(0))
+croplist[["oil.seeds"]]<- list(c(0),c(0))
+croplist[["onion"]]<- list(c(20,46),c(23,49)) # vegetables as indicator, asign to others
+croplist[["palms"]]<- list(c(0),c(0))
+croplist[["peanut"]]<- list(c(11,37),c(11,37)) #groundnuts
+croplist[["peas"]]<- list(c(5,31),c(5,31)) #pulses
+croplist[["pepper"]]<- list(c(20,46),c(23,49)) # vegetables as indicator, asign to others
+croplist[["pine"]]<- list(c(0),c(0))
+croplist[["pineapple"]]<- list(c(23,49),c(23,49)) #others
+croplist[["potatoes"]]<- list(c(7,33),c(7,33))
+croplist[["rice"]]<- list(c(2,28),c(2,28))
+croplist[["rubber"]]<- list(c(0),c(0))
+croplist[["sesame"]]<- list(c(23,29),c(23,29)) #others
+croplist[["sorghum"]]<- list(c(4,30),c(4,30)) #trop cer
+croplist[["soybeans"]]<- list(c(10,36),c(10,36))
+croplist[["sugar.cane"]]<- list(c(13,39),c(13,39))
+croplist[["sunflower"]]<- list(c(9,35),c(9,35))
+croplist[["tea"]]<- list(c(0),c(0))
+croplist[["teak"]]<- list(c(0),c(0))
+croplist[["tomatoes"]]<- list(c(20,46),c(23,49)) # vegetables as indicator, asign to others
+croplist[["trees"]]<- list(c(0),c(0)) 
+croplist[["vegetables"]]<- list(c(20,46),c(23,49)) # vegetables as indicator, asign to others
+croplist[["wheat"]]<- list(c(1,27),c(1,27)) #temp cer
--- land_grab_cft.r
+++ land_grab_cft.r
@@ -6,30 +6,34 @@
  source("~/workspace/LPJ_Utilities/src/map.r")
  source("crops2band.r")
  library(maps)
-
- Reload_cft<-F
+ library(fields)
  
- THISCROP_COUNT<-"ONLY_IRRIG" #"ONLY_RAINFED", ONLY_IRRIG
+ DEBUG<-F
+ Reload_cft<-FALSE
+ PIORITY<-"IRRIG" #"RAINFED", "NOPIRO"
  NUM_CROPS<-26
  NUM_TOTAL_CROPS<-52
  
  grid.file<-"/home/sinan/workspace/LPJmL/LPJmL_Global/inputs2013/grid.bin"
  cft.file<-"/home/sinan/workspace/input_downscale/INPUTS/cft1700_2010_new.bin"
- cow.file<-"/home/sinan/workspace/input_downscale/INPUTS/cow_mg_2006_full.bin"
+ cow.file<-"cow_mg_2006_full.bin"
 
- cft.name52<-c("Temperate_Cereals(r)","Rice(r)","Maize(r)","Tropical_Cereals(r)","Pulses(r)","Temperate_Roots(r)","Potatoes(r)","Tropical_Roots(r)","Sunflower(r)",
-  "Soybeans(r)","Groundnuts(r)","Rapeseed(r)","Sugar_Cane(r)","Citrus(r)",
-  "Non_Citrus_Orchards(r)","Date_Palm(r)","Olives(r)","Nuts_Trees(r)",
-  "Grapes(r)","Vegetables(r)","Cotton(r)","Fodder_grass(r)","Others(r)",
-  "Manage_Grasslands(r)","Bioenergy_Grass(r)","Bioenergy_Tree(r)",
+ cft.name52<-c("Temperate_Cereals_r","Rice_r","Maize_r","Tropical_Cereals_r","Pulses_r","Temperate_Roots_r","Potatoes_r","Tropical_Roots_r","Sunflower_r",
+  "Soybeans_r","Groundnuts_r","Rapeseed_r","Sugar_Cane_r","Citrus_r",
+  "Non_Citrus_Orchards_r","Date_Palm_r","Olives_r","Nuts_Trees_r",
+  "Grapes_r","Vegetables_r","Cotton_r","Fodder_grass_r","Others_r",
+  "Manage_Grasslands_r","Bioenergy_Grass_r","Bioenergy_Tree_r",
   
-"Temperate_Cereals(i)","Rice(i)","Maize(i)","Tropical_Cereals(i)","Pulses(i)",
-"Temperate_Roots(i)","Potatoes(i)","Tropical_Roots(i)","Sunflower(i)",
-  "Soybeans(i)","Groundnuts(i)","Rapeseed(i)","Sugar_Cane(i)","Citrus(i)",
-  "Non_Citrus_Orchards(i)","Date_Palm(i)","Olives(i)","Nuts_Trees(i)",
-  "Grapes(i)","Vegetables(i)","Cotton(i)","Fodder_grass(i)","Others(i)",
-  "Manage_Grasslands(i)","Bioenergy_Grass(i)","Bioenergy_Tree")
+"Temperate_Cereals_i","Rice_i","Maize_i","Tropical_Cereals_i", "Pulses_i",
+"Temperate_Roots_i","Potatoes_i","Tropical_Roots_i","Sunflower_i",
+  "Soybeans_i","Groundnuts_i","Rapeseed_i","Sugar_Cane_i","Citrus_i",
+  "Non_Citrus_Orchards_i","Date_Palm_i","Olives_i","Nuts_Trees_i",
+  "Grapes_i","Vegetables_i","Cotton_i","Fodder_grass_i","Others_i",
+  "Manage_Grasslands_i","Bioenergy_Grass_i","Bioenergy_Tree")
  
+ allcrop_band<-which(cft.name52!="Fodder_grass_i"&cft.name52!="Manage_Grasslands_i"&cft.name52!="Fodder_grass_r"&cft.name52!="Manage_Grasslands_r")
+ 
+ grass_band<-c(22,24,48,50)
  
  COL<- colorRampPalette(c("darkgreen","chartreuse","lemonchiffon","goldenrod1","goldenrod2","goldenrod3","goldenrod"))
  #---------
@@ -58,35 +62,26 @@ if(Reload_cft==TRUE){
 	}
 	save(cft,file="cft.Rdata")
 }else{
-	#load("cft.Rdata")
+	load("cft.Rdata")
 }
 
 #=========
 #load land grab information
 #=========
-landgrab_table<-read.csv("crops.prod.af.csv")
-crop_name<-unique(as.character(landgrab_table$crop))
-landgrab_list<-list()
-for(i in 1:length(crop_name)){
-	landgrab_list[[crop_name[i]]]<-list()
-	landgrab_list[[crop_name[i]]][["cow_iso3"]]<-as.vector(as.character(
+loadLandGrabList<-function(){
+	landgrab_table<-read.csv("crops.prod.af.csv")
+	crop_name<<-unique(as.character(landgrab_table$crop))
+	landgrab_list<-list()
+	for(i in 1:length(crop_name)){
+		landgrab_list[[crop_name[i]]]<-list()
+			landgrab_list[[crop_name[i]]][["cow_iso3"]]<-as.vector(as.character(
 												landgrab_table$ISO3[landgrab_table[,"crop"]==crop_name[i]]))
-	landgrab_list[[crop_name[i]]][["hectares"]]<-as.vector(as.numeric(
+			landgrab_list[[crop_name[i]]][["hectares"]]<-as.vector(as.numeric(
 												landgrab_table$hectares[landgrab_table[,"crop"]==crop_name[i]]))
-	if( THISCROP_COUNT=="ALL"){
-		landgrab_list[[crop_name[i]]][["lpj_band"]]<-as.vector(croplist[[crop_name[i]]])
-	}else{
-		if(THISCROP_COUNT=="ONLY_IRRIG"){
-			indcount<-which(croplist[[crop_name[i]]]>NUM_CROPS)
-		}
-		if(THISCROP_COUNT=="ONLY_RAINFED"){
-			indcount<-which(croplist[[crop_name[i]]]<=NUM_CROPS)
-		}
-		if(length(indcount)==0){
-			landgrab_list[[crop_name[i]]][["lpj_band"]]<-0
-		}else{
-			landgrab_list[[crop_name[i]]][["lpj_band"]]<-as.vector(croplist[[crop_name[i]]][indcount])
-		}
+			landgrab_list[[crop_name[i]]][["band_search"]]<- croplist[[crop_name[i]]][[1]]
+			landgrab_list[[crop_name[i]]][["band_alloc"]] <- croplist[[crop_name[i]]][[2]]
+	}
+	return(landgrab_list)
 }
 	
 #=======
@@ -105,26 +100,46 @@ getThisCountry<-function(cft,cow,this.country){
 	return(this.country.cft)
 }
 
-getThisBandCrops<-function(countrycft, thisband){
+getThisBandArea<-function(countrycft,thisband){
 	this.band.cft<-list()
-	chosen.ind<<-which(countrycft[["cft"]][,thisband]>0,arr.ind=2)
- 	if(is.null(dim(chosen.ind))==FALSE)   chosen.ind<-unique(chosen.ind[,1])
-	
- 	if(length(chosen.ind)==0) return(NULL)
- 	if(length(chosen.ind)==1)  this.band.cft[["cft"]]<-sum(countrycft[["cft"]][chosen.ind,])
- 	else 	this.band.cft[["cft"]]<-rowSums(countrycft[["cft"]][chosen.ind,])
+	chosen.ind<<-which(countrycft[["cft"]][,thisband]>0 & countrycft[["cft"]][,thisband]<0.9,arr.ind=2)
+ 	if(is.null(dim(chosen.ind))==FALSE)   chosen.ind<-unique(chosen.ind[,1])	
+ 	totalfrac<-rowSums(countrycft[["cft"]])
+ 	chosen.ind<-intersect(chosen.ind,which(totalfrac<0.9))
+ 	#if(length(chosen.ind)==1)  this.band.cft[["cft"]]<-sum(countrycft[["cft"]][chosen.ind,])
+ 	
+ 	if(length(chosen.ind)==0) {
+ 		#cat("No", cft.name52[thisband],"!\n")
+ 		return(NULL)
+ 	}
+ 	
+ 	else 	this.band.cft[["cft"]]<-totalfrac[chosen.ind]
 	this.band.cft[["lon"]]<-countrycft[["lon"]][chosen.ind]
 	this.band.cft[["lat"]]<-countrycft[["lat"]][chosen.ind]
 	this.band.cft[["area_total"]]<-countrycft[["area_total"]][chosen.ind]
 	this.band.cft[["index_origin"]]<-countrycft[["index_origin"]][chosen.ind]
 	return(this.band.cft)
 
-	}
 }
 
-getHectarOfThisCountry<-function(landgrab_list,this.crop,this.country){
-	hectaresind<-which(landgrab_list[[this.crop]][["cow_iso3"]]==this.country)
-	return(sum(landgrab_list[[this.crop]][["hectares"]][hectaresind]))
+
+getallArea<-function(countrycft){
+	this.band.cft<-list()
+ 	totalfrac<-rowSums(countrycft[["cft"]])
+ 	this.band.cft[["cft"]]<-totalfrac
+	this.band.cft[["lon"]]<-countrycft[["lon"]]
+	this.band.cft[["lat"]]<-countrycft[["lat"]]
+	this.band.cft[["area_total"]]<-countrycft[["area_total"]]
+	this.band.cft[["index_origin"]]<-countrycft[["index_origin"]]
+	return(this.band.cft)
+}
+
+
+
+getGrabbedArea<-function(landgrab_list,this.crop,this.country){
+#	hectaresind<-which(landgrab_list[[this.crop]][["cow_iso3"]]==this.country)
+#	return(sum(landgrab_list[[this.crop]][["hectares"]][hectaresind]))
+	return(landgrab_list[[this.crop]][["hectares"]][which(landgrab_list[[this.crop]][["cow_iso3"]]==this.country)])
 }
 	
 getUsedArea<-function(cft_list){
@@ -138,62 +153,179 @@ getProportion<-function(cft_list){
 	getAvailArea(cft_list)/sum(getAvailArea(cft_list))
 }
 
-assignThisCropPixel<-function(cft_list,bands,totalgrabbed){
-	toassign<-sum(getAvailArea(cft_list))-totalgrabbed
-	cat("available area=", sum(getAvailArea(cft_list)), "||grabbed area=",totalgrabbed,"(hectares).\n")
-	if(toassign<0) stop("not enough space for assigning grabbed land to only
-		     cropping area, make further program for non-cropping area and grassland :-)\n")
-	else toassign<-0
-	
-	area.to.each.cell<-totalgrabbed*getProportion(cft_list)
-	#check length of vectors are the same
-	if(length(area.to.each.cell)!=length(cft_list[["area_total"]])){
-		print(area.to.each.cell)
-		print(cft_list[["area_total"]])
-		stop("length of area.to.each.cell=",length(area.to.each.cell),"cft_list[[area_total]]=",length(cft_list[["area_total"]]))
+
+allocGrabLand<-function(cft_list,alloc_band,toassign){
+	if(toassign!=0){
+		proportion<-getProportion(cft_list)
+		area_avail<-sum(getAvailArea(cft_list))
+		if(DEBUG==TRUE){
+			cft.test<-new.cft
+		}
+		if(area_avail>toassign){
+			
+			for(i in 1:length(alloc_band)){
+				val<-(toassign*proportion)/length(alloc_band)/cft_list[["area_total"]]
+				new.cft[cft_list[["index_origin"]],alloc_band[i]]<<-new.cft[cft_list[["index_origin"]],alloc_band[i]]+val
+				#old cft also being added the assign value so that for next step the assigned value will appeared on old cft.
+				#cft[cft_list[["index_origin"]],alloc_band[i]]<<-cft[cft_list[["index_origin"]],alloc_band[i]]+new.cft[cft_list[["index_origin"]],alloc_band[i]]
+			}
+			cat("Assigning:",sum(val),"*",length(alloc_band),"to",cft.name52[alloc_band])
+			
+			if(DEBUG==TRUE){
+				if(round(sum(new.cft-cft.test))!=round(toassign)){
+					browser()
+					stop("TEST FAILURE: sum(new)!=toassign ",sum(new.cft-cft.test),"/",toassign)
+				}
+				
+			}
+			return(0)		
+		}else{
+			for(i in 1:length(alloc_band)){
+				val<-(area_avail*proportion)/length(alloc_band)/cft_list[["area_total"]]
+				new.cft[cft_list[["index_origin"]],alloc_band[i]]<<-new.cft[cft_list[["index_origin"]],alloc_band[i]]+val
+				#cft[cft_list[["index_origin"]],alloc_band[i]]<<-cft[cft_list[["index_origin"]],alloc_band[i]]+new.cft[cft_list[["index_origin"]],alloc_band[i]]
+			}
+			cat("Assigning:",sum(val),"*",length(alloc_band),"to",cft.name52[alloc_band])
+			if(DEBUG==TRUE){
+				check_area<-sum(new.cft-cft.test)
+				if(round(check_area)!=round(area_avail)){ 
+					browser() 
+					stop("TEST FAILURE: sum(new)!=toassign ",check_area,"/",area_avail)}
+				}
+			
+			return(toassign-area_avail)		
+		}
+	}else{
+		return(0)
 	}
-	#area to frac
-	frac.to.each.cell<-area.to.each.cell/cft_list[["area_total"]]
-	frac.to.each.cell.each.band<-frac.to.each.cell/length(bands)
-	for(band in bands){
-		new.cft[cft_list[["index_origin"]],band]<<-new.cft[cft_list[["index_origin"]],band]+frac.to.each.cell.each.band
-	}
-	return(toassign)
-	
+
 	
 	
 }
+
+assignTheCropArea<-function(countrycft,landgrab_list,crop,country){
+	to_assign<-getGrabbedArea(landgrab_list,crop,country)
+	band_search<-landgrab_list[[crop]][["band_search"]]
+	band_alloc<-landgrab_list[[crop]][["band_alloc"]]
+	band_search_irrig<-band_search[band_search>NUM_CROPS]
+	band_search_rainf<-band_search[band_search<=NUM_CROPS]
+	band_alloc_irrig<-band_alloc[band_alloc>NUM_CROPS]
+	band_alloc_rainf<-band_alloc[band_alloc<=NUM_CROPS]
+	
+	if(PIORITY=="IRRIG"){
+		cat("1st -> ")
+		have_irrig_list<-getThisBandArea(countrycft,band_search_irrig)
+		have_irrig_availarea<-getAvailArea(have_irrig_list)
+		if(DEBUG==TRUE){
+			if(any(have_irrig_list$cft<0) || any(have_irrig_list$cft>=1)) {stop("\n")}
+			if(any(have_irrig_availarea<0)) {stop("available area < 0 \n")}
+		}
+		cat("Area [only irrigated]:",sum(have_irrig_availarea),"hectares     ")		
+		to_assign<-allocGrabLand(have_irrig_list,band_alloc_irrig,to_assign)
+		cat("......(",to_assign,")\n",sep="")
+
+		cat("2nd -> ")
+		have_rainf_list<-getThisBandArea(countrycft,band_search_rainf)
+		have_rainf_availarea<-getAvailArea(have_rainf_list)
+		if(DEBUG==TRUE){
+			if(any(have_rainf_list$cft<0) || any(have_rainf_list$cft>=1)) {stop("\n")}
+			if(any(have_rainf_availarea<0)) {stop("available area < 0 \n")}
+		}
+		cat("Area [only rainfed]:",sum(have_rainf_availarea),"hectares     ")	
+		to_assign<-allocGrabLand(have_rainf_list,band_alloc_rainf,to_assign)
+		cat("......(",to_assign,")\n",sep="")
+		
+		cat("3rd -> ")
+		have_allcrop_list<<-getThisBandArea(countrycft,allcrop_band)
+		have_allcrop_availarea<-getAvailArea(have_allcrop_list)
+		if(DEBUG==TRUE){
+			if(any(have_allcrop_list$cft<0) || any(have_allcrop_list$cft>=1)) {stop("\n")}
+			if(any(have_allcrop_availarea<0)) {stop("available area < 0 \n")}
+		}
+		cat("Area [all crops]:",sum(have_allcrop_availarea),"hectares     ")	
+		to_assign<-allocGrabLand(have_allcrop_list,band_alloc_irrig,to_assign) # assign to irrigated 
+		cat("......(",to_assign,")\n",sep="")
+		
+		cat("4rd -> ")
+		have_mgrass_list<<-getThisBandArea(countrycft,24) #band 24 rainfed managed grassland
+		have_mgrass_availarea<-getAvailArea(have_mgrass_list)
+		if(DEBUG==TRUE){
+			if(any(have_mgrass_list$cft<0) || any(have_mgrass_list$cft>=1)) {stop("\n")}
+			if(any(have_mgrass_availarea<0)) {stop("available area < 0 \n")}
+		}
+		cat("Area [managed grassland]:",sum(have_mgrass_availarea),"hectares     ")	
+		to_assign<-allocGrabLand(have_allcrop_list,band_alloc_irrig,to_assign) # assign to irrigated 
+		cat("......(",to_assign,")\n",sep="")
+		
+		
+		cat("5th -> ")
+		have_all_list<-getallArea(countrycft)
+		have_all_availarea<-getAvailArea(have_all_list)
+		if(DEBUG==TRUE){
+			if(any(have_allcrop_list$cft<0) || any(have_allcrop_list$cft>=1)) {stop("\n")}
+			if(any(have_allcrop_availarea<0)) {stop("available area < 0 \n")}
+		}
+		cat("Area [entire country]:",sum(have_all_availarea),"hectares     ")	
+		to_assign<-allocGrabLand(have_all_list,c(1:NUM_TOTAL_CROPS),to_assign) # assign to irrigated 
+		cat("......(",to_assign,")\n",sep="")		
+	}
+	
+	
+	if(PIORITY=="RAINFED"){
+		
+	}
+	if(PIORITY=="NOPIRO"){
+		
+	}
+	
+	cat("\n")
+	if(to_assign!=0) stop("to_assign != 0")
+	
+
+}
+
 
 #=====
 #main
 #=====
 new.cft<<-array(0,dim=dim(cft))
+landgrab_list<-loadLandGrabList()
 for(this.crop in crop_name){
 	cat("----------------------","\n")		
-	cat("Assigning the grabbed land of", this.crop,"\n")
+	cat("Assigning: [[", this.crop,"]]\n",sep="")
+	cat("Search Band:",cft.name52[landgrab_list[[this.crop]][["band_search"]]],"\n")
+	cat("Allocate Band:",cft.name52[landgrab_list[[this.crop]][["band_alloc"]]],"\n")
 	cat("----------------------","\n")	
 	for(this.country in landgrab_list[[this.crop]][["cow_iso3"]]){
-		grabbed.hectares<-getHectarOfThisCountry(landgrab_list,this.crop,this.country)
+		grabbed.hectares<-getGrabbedArea(landgrab_list,this.crop,this.country)
 		this.country.name<-as.character(country_code_table$name[which(country_code_table$iso3==this.country)])
 		if(length(this.country.name)!=1) stop("country code is not right: ", this.country)
 		this.lpj.cow<-as.numeric(country_code_table$cow[which(country_code_table$iso3==this.country)])
-		cat(this.country.name, this.country,"(",this.lpj.cow,")","\n")
+		cat(this.country.name, this.country,"(",this.lpj.cow,") \nGrabbed Area:", grabbed.hectares,"hectares.\n")
 		
 		this.country.cft<-getThisCountry(cft=cft,cow=cow,this.country=this.lpj.cow)
 		
+		
+		
 		#1st fill pixels that contain this crop
-		if(landgrab_list[[this.crop]][["lpj_band"]][1]!=0){#no action taken for crops don't have a corresponding LPJ band
-			this.country.this.band.cft<-getThisBandCrops(countrycft=this.country.cft,
-															thisband=landgrab_list[[this.crop]][["lpj_band"]])
-			if(is.null(this.country.this.band.cft)==FALSE){
-			assignThisCropPixel(cft_list=this.country.this.band.cft,
-								      bands=landgrab_list[[this.crop]][["lpj_band"]],
-								      totalgrabbed=grabbed.hectares)
-			
-			}
-				
+		if(landgrab_list[[this.crop]][["band_search"]][1]!=0){#no action taken for crops don't have a corresponding LPJ band
+			left.hectares<-assignTheCropArea(this.country.cft,landgrab_list,this.crop,this.country)
 		}
+
+
+
+
+#			this.country.this.band.cft<-getThisBandCrops(countrycft=this.country.cft,landgrab_list=landgrab_list)
+#			if(is.null(this.country.this.band.cft)==FALSE){
+#			assignThisCropPixel(cft_list=this.country.this.band.cft,
+#								      bands=landgrab_list[[this.crop]][["lpj_band"]],
+#								      totalgrabbed=grabbed.hectares)
+			
+#			}
+				
+#		}
 	}
+	cat("\n\n")
 }
 	
 
@@ -211,5 +343,60 @@ for(i in 1:52){
  }
 
 
+ #=========================trash
  
+ #	if( THISCROP_COUNT=="ALL"){
+#		landgrab_list[[crop_name[i]]][["lpj_band"]]<-as.vector(croplist[[crop_name[i]]])
+#	}else{
+#		if(THISCROP_COUNT=="ONLY_IRRIG"){
+#			indcount<-which(croplist[[crop_name[i]]]>NUM_CROPS)
+#		}
+#		if(THISCROP_COUNT=="ONLY_RAINFED"){
+#			indcount<-which(croplist[[crop_name[i]]]<=NUM_CROPS)
+#		}
+#		if(length(indcount)==0){
+#			landgrab_list[[crop_name[i]]][["lpj_band"]]<-0
+#		}else{
+#			landgrab_list[[crop_name[i]]][["lpj_band"]]<-as.vector(croplist[[crop_name[i]]][indcount])
+#		}
+#	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+#assignThisCropPixel<-function(cft_list,bands,totalgrabbed){
+#	toassign<-sum(getAvailArea(cft_list))-totalgrabbed
+#	cat("available area=", sum(getAvailArea(cft_list)), "||grabbed area=",totalgrabbed,"(hectares).\n")
+#	if(toassign<0) stop("not enough space for assigning grabbed land to only
+#		     cropping area, make further program for non-cropping area and grassland :-)\n")
+#	else toassign<-0
+#	
+#	area.to.each.cell<-totalgrabbed*getProportion(cft_list)
+#	#check length of vectors are the same
+#	if(length(area.to.each.cell)!=length(cft_list[["area_total"]])){
+#		print(area.to.each.cell)
+#		print(cft_list[["area_total"]])
+#		stop("length of area.to.each.cell=",length(area.to.each.cell),"cft_list[[area_total]]=",length(cft_list[["area_total"]]))
+#	}
+#	#area to frac
+#	frac.to.each.cell<-area.to.each.cell/cft_list[["area_total"]]
+#	frac.to.each.cell.each.band<-frac.to.each.cell/length(bands)
+#	for(band in bands){
+#		new.cft[cft_list[["index_origin"]],band]<<-new.cft[cft_list[["index_origin"]],band]+frac.to.each.cell.each.band
+#	}
+#	return(toassign)
+#}
+
+
+
+
 
